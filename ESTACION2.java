9a98f7bd496bb4ba14eefd979cdee0c1bc8905b6
saeli99 <48729343+saeli99@users.noreply.github.com>
2019-04-15 10:02:27 -0500

    Add files via upload

---
+++ ESTACION2.java
@@ -0,0 +1,61 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package estacion2;
+
+/**
+ *
+ * @author PC09-LAB02
+ */
+public class ESTACION2 {
+
+    /**
+     * @param args the command line arguments
+     */
+    public static void main(String[] args) {
+        int mes=8;
+        switch (mes){
+            case 1:
+            case 2:
+                System.out.println("ESTAS EN INVIERNO");
+                break;
+            case 3:
+            case 4:
+            case 5:
+                System.out.println("ESTAS EN PRIMAVERA");
+                break;
+            case 6:
+            case 7:
+            case 8:
+                System.out.println("ESTAS EN VERANO");
+                break;
+            case 9:
+            case 10:
+            case 11:
+                System.out.println("ESTAS EN OTOÑO");
+                break;
+            case 12:
+                System.out.println("ESTAS EN INVIERNO");
+            default:
+                System.out.println("MES Y ESTACION NO EXISTEN");
+                
+                        
+            
+            
+                    
+                        
+                
+                    
+        }
+                
+            
+                
+            
+            
+        
+        // TODO code application logic here
+    }
+    
+}
