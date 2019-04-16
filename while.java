7b4c78cfdc380dceb479a05c1db71c805651f69b
saeli99 <48729343+saeli99@users.noreply.github.com>
2019-04-16 11:58:08 -0500

    Add files via upload

---
+++ while.java
@@ -0,0 +1,34 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package javaapplication1;
+
+/**
+ *
+ * @author PC09-LAB02
+ */
+public class JavaApplication1 {
+
+    /**
+     * @param args the command line arguments
+     */
+    public static void main(String[] args) {
+        int n= 500;
+        int c=1;
+         while (c<=n){
+             System.out.println(c);
+             c++;
+          
+           
+           
+           
+       }
+        
+        
+       
+        // TODO code application logic here
+    }
+    
+}
